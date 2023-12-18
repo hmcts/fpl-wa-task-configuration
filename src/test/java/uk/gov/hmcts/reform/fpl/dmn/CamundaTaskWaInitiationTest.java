79f058d6dbd7c725cb4609f5abdcca61408cc91a
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2023-12-18 10:59:31 +0000

    fix tests

--- src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaInitiationTest.java
@@ -6,7 +6,9 @@ import org.camunda.bpm.engine.variable.VariableMap;
 import org.camunda.bpm.engine.variable.impl.VariableMapImpl;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.hmcts.reform.fpl.DmnDecisionTable;
 import uk.gov.hmcts.reform.fpl.DmnDecisionTableBaseUnitTest;
 
@@ -29,8 +31,8 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
     }
 
     // TODO: Re-enable post-evaluation period
-    // @ParameterizedTest
-    // @MethodSource("scenarioProvider")
+     @ParameterizedTest
+     @MethodSource("scenarioProvider")
     void givenInputShouldReturnOutcomeDmn(String eventId,
                                                Map<String, String> additionalData,
                                                Map<String, ? extends Serializable> expectedDmnOutcome) {
@@ -152,6 +154,6 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
         // The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         // todo - check this after evaluation period
-        assertThat(logic.getRules().size(), is(0));
+        assertThat(logic.getRules().size(), is(20));
     }
 }
