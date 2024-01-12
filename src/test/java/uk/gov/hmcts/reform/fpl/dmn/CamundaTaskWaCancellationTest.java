06fe8cc0508afe2edb79413bcb986d3f97847cbd
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2024-01-12 09:37:55 +0000

    fix checkstyle

--- src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaCancellationTest.java
+++ src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaCancellationTest.java
@@ -17,7 +17,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
-import static java.util.Collections.singletonList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
