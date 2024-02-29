539c34f7ada40f6c13195d6181e8e6b37bcfc3c5
Ashley Wong <ashley.wong@justice.gov.uk>
2024-02-29 16:35:21 +0000

    Stage DFPL-2101 to DEMO

--- src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaInitiationTest.java
@@ -144,6 +144,28 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
                     "name", "Review Order",
                     "processCategories", MANAGE_OUTCOME.getValue()
                 )
+            ),
+            Arguments.of(
+                "create-work-allocation-task",
+                Map.of(
+                    "lastCreatedWATask", "CMO_REVIEWED"
+                ),
+                Map.of(
+                    "taskId", "reviewOrderCMO",
+                    "name", "Review Order",
+                    "processCategories", MANAGE_OUTCOME.getValue()
+                )
+            ),
+            Arguments.of(
+                "create-work-allocation-task",
+                Map.of(
+                    "lastCreatedWATask", "URGENT_CMO_REVIEWED"
+                ),
+                Map.of(
+                    "taskId", "reviewOrderCMOUrgent",
+                    "name", "Review Order (Urgent)",
+                    "processCategories", MANAGE_OUTCOME.getValue()
+                )
             )
         );
     }
@@ -153,6 +175,6 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
         // The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         // todo - check this after evaluation period
-        assertThat(logic.getRules().size(), is(20));
+        assertThat(logic.getRules().size(), is(21));
     }
 }
