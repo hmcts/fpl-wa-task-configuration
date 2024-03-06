18d30f56f634da90f9c0502e3c1121033eec2fcb
Ashley Wong <ashley.wong@justice.gov.uk>
2024-03-06 11:08:36 +0000

    202403061108 Stage DFPL-1893 to DEMO

--- src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaConfigurationTest.java
@@ -63,7 +63,7 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
     void shouldHaveCorrectNumberOfRules() {
         // The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
-        assertThat(logic.getRules().size(), is(64));
+        assertThat(logic.getRules().size(), is(65));
     }
 
     private static List<Map<String, Object>> getBaseValues() {
