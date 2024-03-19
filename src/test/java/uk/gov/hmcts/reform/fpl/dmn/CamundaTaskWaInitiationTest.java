33b0839375f3fc00f641b3c99a7f2570f73339bd
Ashley Wong <ashley.wong@justice.gov.uk>
2024-03-19 11:17:21 +0000

    update test case

--- src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaInitiationTest.java
@@ -175,6 +175,6 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
         // The purpose of this test is to prevent adding new rows without being tested
         DmnDecisionTableImpl logic = (DmnDecisionTableImpl) decision.getDecisionLogic();
         // todo - check this after evaluation period
-        assertThat(logic.getRules().size(), is(26));
+        assertThat(logic.getRules().size(), is(25));
     }
 }
