b1c0dddae0b3b4d27d46df5488e08d1020286bcd
Ashley Wong <ashley.wong@justice.gov.uk>
2024-03-19 11:27:06 +0000

    Revert reviewOrderUrgent

--- src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaInitiationTest.java
+++ src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaInitiationTest.java
@@ -155,17 +155,6 @@ class CamundaTaskWaInitiationTest extends DmnDecisionTableBaseUnitTest {
                     "name", "Review Order",
                     "processCategories", MANAGE_OUTCOME.getValue()
                 )
-            ),
-            Arguments.of(
-                "create-work-allocation-task",
-                Map.of(
-                    "lastCreatedWATask", "URGENT_CMO_REVIEWED"
-                ),
-                Map.of(
-                    "taskId", "reviewOrderCMOUrgent",
-                    "name", "Review Order (Urgent)",
-                    "processCategories", MANAGE_OUTCOME.getValue()
-                )
             )
         );
     }
