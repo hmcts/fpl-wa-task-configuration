a6b33c4bdfaa820980f121d35e7fe796ff672680
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2024-10-22 15:45:04 +0100

    DFPL-2526: Update task prioritisation (#235)
    * DFPL-2526: Update task prioritisation
    
    * DFPL-2526: Update test values
    
    * DFPL-2526: Should not be reconfigurable attributes
    
    * fix reconfigure test

--- src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaConfigurationTest.java
+++ src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaConfigurationTest.java
@@ -70,7 +70,7 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
         DmnDecisionTableResult dmnDecisionTableResult = evaluateDmnTable(inputVariables);
         assertTrue(dmnDecisionTableResult.getResultList().containsAll(List.of(
             getRowResult("minorPriority", "500", true),
-            getRowResult("majorPriority", "5000", true)
+            getRowResult("majorPriority", "5000", false)
         )));
     }
 
@@ -82,11 +82,11 @@ class CamundaTaskWaConfigurationTest extends DmnDecisionTableBaseUnitTest {
 
     private static Stream<Arguments> approveOrdersMajorPriorityScenarios() {
         return Stream.of(
-            Arguments.of(1000, Map.of("draftOrderUrgency", Map.of("urgency", List.of("YES")))),
-            Arguments.of(4000, Map.of("draftOrderUrgency", Map.of("urgency", List.of("NO")))),
-            Arguments.of(4000, Map.of("draftOrderUrgency", toNullValueMap("urgency"))),
-            Arguments.of(4000, toNullValueMap("draftOrderUrgency")),
-            Arguments.of(4000, null)
+            Arguments.of(2000, Map.of("draftOrderUrgency", Map.of("urgency", List.of("YES")))),
+            Arguments.of(5000, Map.of("draftOrderUrgency", Map.of("urgency", List.of("NO")))),
+            Arguments.of(5000, Map.of("draftOrderUrgency", toNullValueMap("urgency"))),
+            Arguments.of(5000, toNullValueMap("draftOrderUrgency")),
+            Arguments.of(5000, null)
         );
     }
 
