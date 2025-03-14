d3d235282b3c28bef4b4b4c4c3f7874f0f41198c
Daniel Catchpole <daniel.catchpole@justice.gov.uk>
2025-03-14 12:14:15 +0000

    Revert "DFPL-2669: Disable manual completion for judicial/local court tasks (#289)"
    This reverts commit d2f9ebda1c9e2132bce0025085a1a0a7d674bdb2.

--- src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaPermissionsTest.java
+++ src/test/java/uk/gov/hmcts/reform/fpl/dmn/CamundaTaskWaPermissionsTest.java
@@ -52,7 +52,7 @@ class CamundaTaskWaPermissionsTest extends DmnDecisionTableBaseUnitTest {
                 ),
                 getRowResult(
                     "allocated-judge",
-                    "Own,Assign,Claim,Unassign,Read",
+                    "Complete,Own,Assign,Claim,Unassign,Read",
                     "JUDICIAL",
                     "316",
                     1,
@@ -60,7 +60,7 @@ class CamundaTaskWaPermissionsTest extends DmnDecisionTableBaseUnitTest {
                 ),
                 getRowResult(
                     "tribunal-caseworker",
-                    "Own,Assign,Claim,Unassign,Read",
+                    "Complete,Own,Assign,Claim,Unassign,Read",
                     "LEGAL_OPERATIONS",
                     null,
                     null,
